6f7cc117c091c004b15dfeba9d19d9607530631a
Michal Stabrawa <mistaaqua@gmail.com>
2018-07-05 19:48:29 +0200

    add

---
+++ app/js/main.js
@@ -0,0 +1,29 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:true
+        }
+    }
+})
+
+ $(document).on('click', 'a[href^="#"]', function(event) {
+       event.preventDefault();
+var menuHeight = $('#main-nav').height();
+       $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
+        }, 500);
+  });
