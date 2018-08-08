17d68aed5096d224d83a28d23ac24315482b1b41
Michal Stabrawa <mistaaqua@gmail.com>
2018-08-08 07:33:55 +0200

    rwd

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
-$('.owl-carousel').owlCarousel({
+
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
     loop:true,
-    autoplay:true,
-    margin:10,
+    autoplay:false,
+    margin:0,
     responsiveClass:true,
     responsive:{
         0:{
@@ -14,7 +18,7 @@ $('.owl-carousel').owlCarousel({
         },
         1000:{
             items:2,
-            nav:true,
+            nav:false,
             loop:true
         }
     }
@@ -27,3 +31,46 @@ var menuHeight = $('#main-nav').height();
             scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
         }, 500);
   });
+    
+    
+    setTimeout(function(){
+   
+        $('h1').addClass('animated fadeInDown');
+    },1000);
+    
+    setTimeout(function(){
+        $('.jumbotron').find('h2').addClass('animated fadeInLeft');
+    },1700);
+    
+      setTimeout(function(){
+        $('.jumbotron').find('a.btn').addClass('animated fadeInUp');
+    },2600);
+    
+    
+    $.get( "http://api.apixu.com/v1/forecast.json?key=87a41020a4164a21b50181358182407&q=Paris", function( data ) {
+  $( ".result" ).html( data );
+        var dane=data;
+        console.log(data);
+  alert( "Load was performed." );
+        
+        document.querySelector('.weather').innerHTML=data.location.name;
+        
+});
+    
+    
+       $.get( "http://api.nbp.pl/api/exchangerates/rates/a/chf/", function( data ) {
+  $( ".result" ).html( data );
+        var dane=data;
+        console.log(data);
+  alert( "Load was performed." );
+        var dane1=JSON.parse(dane.rates);
+           console.log("json?" +":"+ dane1)
+
+        
+});
+    
+    $('#btn-first').click(function(){
+        $("html, body").animate({ scrollTop: $('.how-work').height() }, 2000);
+    })
+})
+
