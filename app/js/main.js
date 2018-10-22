0b68305470c2597d5f5aa740edc6ba898a09a3a0
Michal Stabrawa <mistaaqua@gmail.com>
2018-10-22 20:27:56 +0200

    wyslij

--- app/js/main.js
+++ app/js/main.js
@@ -1,69 +1,61 @@
-
-
-
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-    loop:true,
-    autoplay:true,
-    margin:0,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false,
-            dots:false,
-        },
-        600:{
-            items:1,
-            nav:false,
-            dots:false,
-        },
-        1000:{
-            items:2,
-            nav:false,
-            loop:true
-        }
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: false
+      },
+      600: {
+        items: 1,
+        nav: false,
+        dots: false
+      },
+      1000: {
+        items: 2,
+        nav: false,
+        loop: true
+      }
     }
-})
-
- $(document).on('click', 'a[href^="#"]', function(event) {
-       event.preventDefault();
-var menuHeight = $('#main-nav').height();
-       $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top - menuHeight
-        }, 500);
   });
-    
-    
-    setTimeout(function(){
-   
-        $('h1').addClass('animated fadeInDown');
-    },1000);
-    
-    setTimeout(function(){
-        $('.jumbotron').find('h2').addClass('animated fadeInLeft');
-    },1700);
-    
-      setTimeout(function(){
-        $('.jumbotron').find('a.btn').addClass('animated fadeInUp');
-    },2600);
-    
-    
 
-    
-    
+  $(document).on("click", 'a[href^="#"]', function(event) {
+    event.preventDefault();
+    var menuHeight = $("#main-nav").height();
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top - menuHeight
+      },
+      500
+    );
+  });
 
-    
-    $('#btn-first').click(function(){
-        $("html, body").animate({ scrollTop: $('.how-work').height() }, 2000);
-    })
-    
-    $('#hamburger').click(function(){
-        $('#main-nav').toggle();
-        $("#main-nav").css("background", "  rgba(73, 59, 84, 0.8);");
+  setTimeout(function() {
+    $("h1").addClass("animated fadeInDown");
+  }, 1000);
 
-    })
-})
+  setTimeout(function() {
+    $(".jumbotron")
+      .find("h2")
+      .addClass("animated fadeInLeft");
+  }, 1700);
 
+  setTimeout(function() {
+    $(".jumbotron")
+      .find("a.btn")
+      .addClass("animated fadeInUp");
+  }, 2600);
 
+  $("#btn-first").click(function() {
+    $("html, body").animate({ scrollTop: $(".how-work").height() }, 2000);
+  });
 
+  $("#hamburger").click(function() {
+    $("#main-nav").toggle();
+    $("#main-nav").css("background", "  rgba(73, 59, 84, 0.8)");
+  });
+});
